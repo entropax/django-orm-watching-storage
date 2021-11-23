b8cb860c33b44380a228a48b51dfc262a2fcb926
Cain Entropax Maxwell <cainmax@posteo.mx>
2021-11-23 17:59:06 +0300

    Modify functions

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 
 
 def active_passcards_view(request):
-    # Программируем здесь
-
     all_passcards = Passcard.objects.all()
     all_active = all_passcards.filter(is_active=True)
     context = {
-        'active_passcards': all_active,  # люди с активными пропусками
+        'active_passcards': all_active,
     }
     return render(request, 'active_passcards.html', context)
