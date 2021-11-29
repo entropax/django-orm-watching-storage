1dc369e9fd9c52d879e2692c73292d828b3812b0
Cain Entropax Maxwell <cainmax@posteo.mx>
2021-11-29 18:14:44 +0300

    Update: use one .env variable for DB_URL

--- project/settings.py
+++ project/settings.py
@@ -1,44 +1,36 @@
 import os
 from environs import Env
+import dj_database_url
 
 env = Env()
 env.read_env()
 
 DEBUG = env.bool("DEBUG")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env("HOST"),
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': env("PASSWORD"),
-    }
-}
 
-INSTALLED_APPS = ['datacenter']
+DATABASES = {"default": env.dj_db_url("DB_URL")}
 
-SECRET_KEY = 'REPLACE_ME'
+INSTALLED_APPS = ["datacenter"]
+
+SECRET_KEY = env("SECRET_KEY")
 
 ROOT_URLCONF = "project.urls"
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
     },
 ]
 
-
 USE_L10N = True
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = "Europe/Moscow"
 
 USE_TZ = True
